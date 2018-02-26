6022484f2c9105ff6b521d645cf1ad303c8e1d53
Victor <vrw1984@gmail.com>
2018-02-26 18:00:59 -0400

    Initial Commit

---
+++ TodoList App with Core Data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoList App with Core Data
+//
+//  Created by Victor on 2018-02-26.
+//  Copyright © 2018 kodechamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
